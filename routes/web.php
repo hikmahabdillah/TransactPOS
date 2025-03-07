d0ed7fe5cebc90b0ef805a0d48b72014d6d1b8ac
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-03-07 09:46:51 +0700

    Jobsheet2 - SoalPraktikum No 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 // HOME
 Route::get('/', [HomeController::class, 'index']);
+
+// PRODUCTS
+Route::prefix('category')->group(function () {
+    Route::get('/', [ProductsController::class, 'index']);
+
+    Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductsController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductsController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductsController::class, 'babyKid']);
+});
