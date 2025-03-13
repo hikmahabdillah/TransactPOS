f5e33bf7a3439376fd515a3cb1212befd94c42a3
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-03-13 13:29:05 +0700

    jobsheet3 - Praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\TransactionController;
@@ -12,6 +13,7 @@ Route::get('/', [HomeController::class, 'index']);
 
 // LEVEL
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 // PRODUCTS
 Route::prefix('category')->group(function () {
