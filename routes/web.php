7a2ad1a30a5c5f88b491f859fd03ef62121492e5
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-05-10 07:41:19 +0700

    Jobsheet8 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -109,6 +109,7 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
     Route::middleware(['authorize:ADM,MNG'])->group(function () {
         Route::group(['prefix' => 'barang'], function () {
             Route::get('/', [BarangController::class, 'index']);
+            Route::post('/list', [BarangController::class, 'list']);
             Route::get('/create', [BarangController::class, 'create']);
             Route::post('/', [BarangController::class, 'store']);
             Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
@@ -121,6 +122,8 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
             Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
             Route::delete('/{id}', [BarangController::class, 'destroy']);
+            Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
+            Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
         });
     });
 
